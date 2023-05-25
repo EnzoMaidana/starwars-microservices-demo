a2e5bb495164a1383212610c02d6cd63962b270a
EnzoMaidana <enzomaidana1995@gmail.com>
2023-05-25 18:13:06 -0300

    Cambios

--- characters/src/controllers/getCharacters.js
+++ characters/src/controllers/getCharacters.js
@@ -3,7 +3,7 @@ const {response} = require('../utils');
 const axios = require('axios');
 
 module.exports = async (req, res) => {
-    const characters = await axios.get("http://database:8004/Character")
+    const characters = await axios.get("http://database:3004/Character")
     response(res, 200, characters);
 };
 
--- planets/src/controllers/getPlanets.js
+++ planets/src/controllers/getPlanets.js
@@ -2,7 +2,7 @@ const Planet = require('../data');
 const { response } = require('../utils');
 
 module.exports = async (req, res) => {
-    const planets = await axios.get("http://database:8004/Planet")
+    const planets = await axios.get("http://database:3004/Planet")
     response(res, 200, planets);
 };
 
